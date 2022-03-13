683a61c480672e49efdf25b25d999d11dc2b8f91
Bakiury <thecutaseta@gmail.com>
2022-03-13 12:10:01 -0500

    Login, SignUp and part of the home page were created

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,116 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="navBox">
+      <router-link to="/">Inicio</router-link>
+      <router-link to="/user/profile">Perfil</router-link>
+    </div>
   </div>
-  <router-view/>
+  <router-view />
+
+  <footer class="footer">
+    <div class="myBoth">
+      <div class="myOneCol">
+        <router-link to="/about">Sobre mi</router-link>
+        <router-link to="/contact">Contáctame</router-link>
+      </div>
+      <div class="myTwoCol">
+        <router-link to="/terms">Términos</router-link>
+        <router-link to="/privacity">Privacidad</router-link>
+      </div>
+    </div>
+    <div class="myThreeCol">
+      <a target="_blank" href="https://www.facebook.com/"><i class="fab fa-facebook-square"></i></a>
+      <a target="_blank" href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+      <a target="_blank" href="https://www.snapchat.com/"><i class="fab fa-snapchat-ghost"></i></a>
+      <a target="_blank" href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+      <a target="_blank" href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
+    </div>
+  </footer>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App"
+});
+</script>
+
+<style scoped>
+#nav {
+  display: flex;
+  justify-content: flex-end;
+  padding: 10px;
+  background: #30c530;
+  box-shadow: 0px 2px 5px 0px #34495e;
 }
 
-#nav {
-  padding: 30px;
+.navBox {
+  display: flex;
+  justify-content: space-around;
+  width: 380px;
+  font-size: 18px;
+  margin-right: 50px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  text-decoration: none;
+  color: #ffffff;
+  border-radius: 10px;
+  padding-right: 15px;
+  padding-left: 15px;
+  padding-top: 5px;
+  padding-bottom: 5px;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+#nav a:hover {
+  background: #ffffff;
+  color: #30c530;
+  transition: all .3s ease;
+}
+
+.footer {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background: #30c530;
+  position: relative;
+  bottom: 0px;
+  height: 100px;
+  width: 100%;
+  box-shadow: 0px -2px 5px 0px #34495e;
+}
+
+.myBoth {
+  display: flex;
+  justify-content: center;
+}
+
+.myOneCol, .myTwoCol {
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+}
+
+.myOneCol a, .myTwoCol a, .myThreeCol a {
+  text-decoration: none;
+  color: #ffffff;
+}
+
+.myOneCol a:hover, .myTwoCol a:hover, .myThreeCol a:hover {
+  color: #34495e;
+  transition: all .3s ease;
+}
+
+.myThreeCol {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 250px;
+  margin-right: -100px;
+}
+
+.myThreeCol a {
+  font-size: 20px;
 }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,176 @@
+<template>
+  <div class="all">
+    <div class="main-section">
+      <div class="modal-content">
+        <div class="user-img">
+          <img src="../assets/user.png" />
+        </div>
+        <form class="" v-on:submit.prevent="processLogInUser" autocomplete="off">
+          <div class="form-group" id="user-group">
+            <div>
+              <i class="fas fa-user"></i>
+            </div>
+            <input
+              id="myUser"
+              type="text"
+              class="form-control" v-model="user.use_email"
+              placeholder="Email"
+              name="username"
+            />
+          </div>
+          <div class="form-group">
+            <div>
+              <i class="fas fa-lock"></i>
+            </div>
+            <input
+              id="myPass"
+              type="password"
+              class="form-control" v-model="user.use_password"
+              placeholder="Contraseña"
+              name="password"
+            />
+          </div>
+          <button type="submit" class="btn">
+            <i class="fas fa-sign-in-alt"></i> Ingresar
+          </button>
+        </form>
+        <div class="col-12 forgot">
+          <router-link to="/user/signUp" class="nav-link active"
+            >No tienes una cuenta registrate aqui</router-link
+          >
+        </div>
+        <div th:if="${param.error}" v-if="msgWrongPass" class="alert alert-danger" role="alert">
+          Credenciales incorrectas.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import $ from "jquery";
+
+export default defineComponent({
+  name: "Login",
+  data: () => {
+    return {
+      user: {
+        use_email: "",
+        use_password: "",
+      },
+      msgWrongPass: false,
+    };
+  },
+  components: {},
+  methods: {
+    hidenav: () => {
+      let myPath = window.location.pathname.split("/")[1].toLowerCase();
+      if (myPath === "login") {
+        $("#nav").hide();
+        $(".footer").hide();
+      }
+    },
+    processLogInUser: () => {
+        return "";
+    }
+  },
+  created: function () {
+    this.hidenav();
+  },
+});
+</script>
+
+<style scoped>
+.all {
+  position: absolute;
+  background-image: url('../assets/background.jpg');
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  font-family: 'Baloo 2', cursive;
+}
+
+.main-section {
+  position: absolute;
+  transform: translate(-50%, -50%);
+  left: 50%;
+  top: 50%;
+  text-align: center;
+}
+
+.modal-content {
+  background-color: rgba(255, 234, 167, .8);
+  padding: 0 56px;
+  width: 385px;
+  box-shadow: 0px 0px 2px 3px #ffffff;
+  border-radius: 20px;
+}
+
+.user-img {
+  margin-top: -50px;
+  margin-bottom: 35px;
+}
+
+.user-img img {
+  width: 100px;
+  height: 100px;
+  box-shadow: 0px 0px 2px 3px #ffffff;
+  border-radius: 50%;
+}
+
+.form-group input {
+  height: 42px;
+  font-size: 18px;
+  border: 0;
+  padding: 25px;
+  padding-left: 60px;
+  border-radius: 5px;
+  color: #34495e;
+  font-weight: bold;
+  margin-bottom: 25px;
+  background-color: #ffffff;
+}
+
+.form-group div {
+  background: #333333;
+  width: 50px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  color: #ffffff;
+}
+
+button {
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  background: #333333;
+  color: #ffffff;
+}
+
+button:hover {
+  color: #ffffff;
+  background: #444444;
+}
+
+.forgot {
+  padding: 5px 0;
+}
+
+.forgot a {
+  font-size: 14px;
+  font-style: italic;
+  font-weight: bold;
+  color: #333333;
+}
+
+.forgot a:hover {
+  color: #444444;
+}
+</style>
